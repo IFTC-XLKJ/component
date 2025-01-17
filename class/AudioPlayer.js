aa3433e1da65043e101cbbaad904120c7fb35870
IFTC-XLKJ <iftcceo@139.com>
2025-01-17 19:55:41 +0800

    114514

--- class/AudioPlayer.js
+++ class/AudioPlayer.js
@@ -6,11 +6,13 @@ class aAudioPlayer {
         muted: false,
         volume: 1,
     };
+    #audio = null;
+    #events = {};
     constructor(element, config) {
-        this.audio = new Audio();
+        this.#audio = new Audio();
         if (config) {
             if (config.src) {
-                this.audio.src = config.src;
+                this.#config.src = config.src;
             }
             if (config.loop) {
                 this.audio.loop = config.loop;
@@ -25,10 +27,36 @@ class aAudioPlayer {
                 this.audio.volume = config.volume;
             }
         }
-        this.audio.addEventListener('ended', () => {
-            this.audio.currentTime = 0;
-            this.audio.play();
-        });
+        this.#audio.src = this.#config.src;
+        this.#audio.onloadedmetadata = e => {
+            this.#emit("load", e);
+            if (this.#config.loop) {
+                this.#audio.loop = true;
+            }
+            if (this.#config.autoplay) {
+                this.#audio.play();
+            }
+            if (this.#config.muted) {
+                this.#audio.muted = true;
+            }
+        }
+    }
+    play() {
+        return this.#audio.play();
+    }
+    pause() {
+        return this.#audio.pause();
+    }
+    on(name, callback) {
+        if (!this.#events[name]) {
+            this.#events[name] = [];
+        }
+        this.#events[name].push(callback);
+    }
+    #emit(name, ...args) {
+        if (this.#events[name]) {
+            this.#events[name].forEach(callback => callback(...args));
+        }
     }
 }
 window.AudioPlayer = aAudioPlayer;
