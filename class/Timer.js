37d233d4aef76daece81c25ebdf7475c6ffebde9
IFTC-XLKJ <iftcceo@139.com>
2025-03-30 11:56:45 +0800

    Update Timer.js

--- class/Timer.js
+++ class/Timer.js
@@ -1,40 +1,40 @@
 class aTimer {
     #timeStart = 0;
     #time = 0;
-    #marked = []
-    _isTiming = false
+    #marked = [];
+    _isTiming = false;
     get isTiming() {
-        this._isTiming = !!this.#timeStart
-        return this._isTiming
+        this._isTiming = !!this.#timeStart;
+        return this._isTiming;
     }
     constructor() { }
     start() {
-        this.#timeStart = Date.now()
+        this.#timeStart = Date.now();
     }
     get() {
         if (!this.#timeStart) {
-            console.error("计时器还未开始计时")
-            return null
+            console.error("计时器还未开始计时");
+            return null;
         }
-        this.#time = Date.now() - this.#timeStart
-        return this.#time
+        this.#time = Date.now() - this.#timeStart;
+        return this.#time;
     }
     mark() {
-        this.#marked.push(this.get())
+        this.#marked.push(this.get());
     }
     getMark() {
-        return this.#marked
+        return this.#marked;
     }
     markCount() {
-        return this.#marked.length
+        return this.#marked.length;
     }
     clear() {
         this.#timeStart = Date.now();
-        this.#marked = []
+        this.#marked = [];
     }
     stop() {
-        this.#time = 0
-        this.#timeStart = 0
+        this.#time = 0;
+        this.#timeStart = 0;
     }
 
     formatTime(milliseconds) {
@@ -44,7 +44,11 @@ class aTimer {
         let ms = milliseconds % 1000;
         return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}.${String(ms).padStart(3, '0')}`;
     }
+
+    toString() {
+        return this.formatTime(this.#time);
+    }
 }
-window.Timer = aTimer
-console.log("Timer.js已加载")
-console.log("文档：/docs/Timer.md")
+window.Timer = aTimer;
+console.log("Timer.js已加载");
+console.log("文档：/docs/Timer.md");
